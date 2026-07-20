10f23e20b6c98eebb73af70025da247c4d19455b
NaveenKumar-777 <naveenkumarsvh777@gmail.com>
2026-07-20 17:58:31 +0530

    spi

--- spi_round.vhd
+++ spi_round.vhd
@@ -1,40 +1,74 @@
-process(clk)
-    variable next_ch : integer;
+brary ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ads127111_spi_channel is
+    port (
+        clk                  : in  std_logic;
+        reset                : in  std_logic;
+
+        adc_reg_wr_en_reg    : in  std_logic_vector(15 downto 0);
+        adc_reg_wr_en_reg_wr : in  std_logic;
+
+        channel_done         : in  std_logic_vector(15 downto 0);
+        channel_done_en      : out std_logic_vector(15 downto 0)
+    );
+end entity;
+
+architecture rtl of ads127111_spi_channel is
+
+    signal current_ch : integer range 0 to 15 := 0;
+    signal ch_en      : std_logic_vector(15 downto 0) := (others => '0');
+
 begin
-    if rising_edge(clk) then
 
-        if reset = '1' then
-            channel_done_en <= (others => '0');
-            current_ch <= 0;
+    channel_done_en <= ch_en;
 
-        -- Load new mask
-        elsif adc_reg_wr_en_reg_wr = '1' then
+    process(clk)
+        variable next_ch : integer range 0 to 15;
+    begin
+        if rising_edge(clk) then
 
-            for i in 0 to 15 loop
-                if adc_reg_wr_en_reg(i) = '1' then
-                    current_ch <= i;
-                    channel_done_en <= (others => '0');
-                    channel_done_en(i) <= '1';
-                    exit;
-                end if;
-            end loop;
+            if reset = '1' then
+                current_ch <= 0;
+                ch_en <= (others => '0');
 
-        -- Current channel finished
-        elsif channel_done(current_ch) = '1' then
+            ----------------------------------------------------------------
+            -- Load new channel mask
+            ----------------------------------------------------------------
+            elsif adc_reg_wr_en_reg_wr = '1' then
 
-            for i in 1 to 16 loop
-                next_ch := (current_ch + i) mod 16;
+                ch_en <= (others => '0');
 
-                if adc_reg_wr_en_reg(next_ch) = '1' then
-                    current_ch <= next_ch;
-                    channel_done_en <= (others => '0');
-                    channel_done_en(next_ch) <= '1';
-                    exit;
-                end if;
-            end loop;
+                for i in 0 to 15 loop
+                    if adc_reg_wr_en_reg(i) = '1' then
+                        current_ch <= i;
+                        ch_en(i) <= '1';
+                        exit;
+                    end if;
+                end loop;
+
+            ----------------------------------------------------------------
+            -- Current channel completed
+            ----------------------------------------------------------------
+            elsif channel_done(current_ch) = '1' then
+
+                ch_en <= (others => '0');
+
+                for i in 1 to 16 loop
+                    next_ch := (current_ch + i) mod 16;
+
+                    if adc_reg_wr_en_reg(next_ch) = '1' then
+                        current_ch <= next_ch;
+                        ch_en(next_ch) <= '1';
+                        exit;
+                    end if;
+                end loop;
+
+            end if;
 
         end if;
-    end if;
-end process;
-:wq
+    end process;
+
+end architecture rtl;
 
